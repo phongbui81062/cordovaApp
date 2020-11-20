e8f59c752389d0aad71a453e9b2777ac5054e2c6
PhongBui <phongbqt1999@gmail.com>
2020-11-20 10:11:11 +0700

    alo

--- Restaurant/www/js/index.js
+++ Restaurant/www/js/index.js
@@ -1,29 +1,84 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+document.addEventListener("deviceready", onDeviceReady, false);
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+var db = "";
+
+function restaurantDB(tx) {
+    tx.executeSql(`create table if not exists Restaurant(restaurantId integer primary key autoincrement,
+        restaurantName text not null,
+        restaurantType text not null,
+        visitDate DATETIME not null default current_timestamp,
+        mealPrice interger not null,
+        serviceRating text not null,
+        cleanRating text not null,
+        foodQuality text not null,
+        Note text,
+        reporterName text not null)`
+        );
+
+}
+
+
+function errorCB(err) {
+    alert("Error processing SQL: "+err.code);
+}
+
+
+function successCB() {
+    alert("alo alo")
+}
+
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    db = window.openDatabase("Restaurant", "1.0", "Restaurant", 200000)
+    db.transaction(restaurantDB, errorCB, successCB);
 }
+
+function insertRestaurant(restaurant){
+    db.transaction(function (tx){
+        var query = `insert into Restaurent(restaurantName,
+                                            restaurantType,
+                                            visitDate,
+                                            mealPrice,
+                                            serviceRating,
+                                            cleanRating,
+                                            foodQuality,
+                                            Note,
+                                            reporterName) Value(?,?,?,?,?,?,?,?,?)`
+        tx.executeSql(query,[restaurant.restaurantName,
+                            restaurant.restaurantType,
+                            restaurant.visitDate,
+                            restaurant.mealPrice,
+                            restaurant.serviceRating,
+                            restaurant.cleanRating,
+                            restaurant.foodQuality,
+                            restaurant.Note,
+                            restaurant.reporterName],function (){
+            console.log("insert done");
+        },transError)
+    })
+}
+// function updateRestaurant(restaurant){
+//     db.transaction(function (tx){
+//         var query = `update Restaurent Set Name=?,Location=? where id =${restaurant.id}`
+//         tx.executeSql(query,[restaurant.Name,restaurant.Location],function (){
+//             console.log("update done");
+//         },transError)
+//     })
+// }
+
+// function searchRestaurant(restaurant){
+//     db.transaction(function (tx){
+//         var query = `select * from Restaurent where Name like "%${restaurant.Name}%",Location=? where id =${restaurant.id}`
+//         tx.executeSql(query,[restaurant.Name,restaurant.Location],function (){
+//             console.log("search done");
+//         },transError)
+//     })
+// }
+// function deleteRestaurant(restaurant){
+//     db.transaction(function (tx){
+//         var query = `delele from Restaurent where id=${restaurant.id}",Location=? where id =${restaurant.id}`
+//         tx.executeSql(query,[],function (){
+//             alert("delete done");
+//         },transError)
+//     })
+// }
